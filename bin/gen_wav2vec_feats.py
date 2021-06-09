0e3f0a795e6d6bb62a4bae4eacf228baea8deffc
Neville Ryant <nryant@gmail.com>
2021-06-08 20:10:10 -0400

    REFACTOR: Substantially clean up wav2vec feat gen code.

--- bin/gen_wav2vec_feats.py
+++ bin/gen_wav2vec_feats.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
-"""Extract acoustic features using wav2vec, vq-wav2vec or wav2vec 2.0 models.
+#!/usr/bin/env python
+"""Extract acoustic features using wav2vec, vq-wav2vec or wav2vec 2.0 model.
 
+wav2vec
+-------
 To extract features using a pretrained wav2vec model:
 
-    python gen_wav2vec_feats.py --use_gpu w2v_model.pt feats_dir/ \
-        fn1.wav fn2.wav fn3.wav ...
+    python gen_wav2vec_feats.py --use-gpu w2v_model.pt feats_dir/ fn1.wav fn2.wav fn3.wav ...
 
 which will load a pretrained model from the checkpoint located at
 
@@ -12,7 +13,7 @@ which will load a pretrained model from the checkpoint located at
 
 apply it to the audio files ``fn1.wav, ``fn2.wav``, ``fn3.wav``, ..., and for
 each recording output frame-level features to a corresponding ``.npy`` file
-located under the directory ``feats_dir``. The flag ``--use_gpu`` instructs
+located under the directory ``feats_dir``. The flag ``--use-gpu`` instructs
 the script to use the GPU, if free.
 
 For each audio file, this script outputs a NumPy ``.npy`` file containing an
@@ -20,28 +21,30 @@ For each audio file, this script outputs a NumPy ``.npy`` file containing an
 correspond to frames sampled every 10 ms starting from an offset of 0; that is,
 the ``i``-th frame of features corresponds to an offset of ``i*0.010`` seconds.
 
-If you would instead like to extract RoBERTa features from a RoBERTa model
-processing quantized features output by a vq-wav2ec model, run:
+vq-wav2vec
+----------
+If you would instead like to extract features from a RoBERTa model processing
+quantized features output by vq-wav2ec, run:
 
-    python gen_wav2vec_feats.py --use_gpu \
-        --roberta roberta_model.pt --vocab dict.txt \
-        vqw2v_model.pt feats_dir/ fn1.wav fn2.wav fn3.wav ...
+    python gen_wav2vec_feats.py --use-gpu --roberta roberta_model.pt --vocab dict.txt  vqw2v_model.pt feats_dir/ fn1.wav fn2.wav fn3.wav ...
 
 where ``vqw2v_model.pt`` is the checkpoint from a pretrained vq-wav2vec model,
 ``roberta_model.pt`` is the corresponding pretrained RoBERTa model checkpoint,
 and ``dict.txt`` is the vocabulary used by that RoBERTa model.
 
-If you would like to extract wav2vec 2.0 features from a wav2vec 2.0 model,
-run:
+wav2vec 2.0
+-----------
+If you would like to extract wav2vec 2.0 features from a pretrained
+wav2vec 2.0 model, run:
 
-    python gen_wav2vec_feats.py --use_gpu --v2 w2v2_model.pt feats_dir/ \
-        fn1.wav fn2.wav fn3.wav ...
+    python gen_wav2vec_feats.py --use-gpu --v2 w2v2_model.pt feats_dir/ fn1.wav fn2.wav fn3.wav ...
+
 where ``w2v2_model.pt`` is the checkpoint from a pretrained wav2vec 2.0 model.
 """
 import argparse
-import os
 from pathlib import Path
 import sys
+
 import fairseq
 from fairseq.data import Dictionary
 from fairseq.models.roberta import RobertaModel
@@ -55,7 +58,6 @@ from wurlitzer import pipes
 
 
 activation = {}
-
 
 def get_activation(name):
     """Extract output of an intermediate layer"""
@@ -75,8 +77,65 @@ def get_device(x):
                          f'{type(x)}')
 
 
+def load_audio(audio_path, device='cpu'):
+    """Return audio as row vector of 16 kHz samples on ``device``."""
+    x, sr = librosa.load(audio_path, sr=16000)
+    x = torch.from_numpy(x).unsqueeze(0)
+    x = x.to(device)
+    return x
+
+
+def extract_wav2vec_feats(audio_path, model):
+    """Extract context aggregation layer features from wav2vec model."""
+    x = load_audio(audio_path, device=get_device(model))
+    with torch.no_grad():
+        z = model.feature_extractor(x)
+        c = model.feature_aggregator(z)
+    return c.cpu().numpy()
+
+
+def extract_vqwav2vec_feats(audio_path, wav2vec_model, roberta_model):
+    """Extract features from a RoBERTa model processing quantized features
+    output by vq-wav2ec.
+    """
+    device = get_device(wav2vec_model)
+    x = load_audio(audio_path, device=device)
+    with torch.no_grad():
+        # Get labels for frame via vector quantization.
+        z = wav2vec_model.feature_extractor(x)
+        c = wav2vec_model.feature_aggregator(z)
+        _, idx = wav2vec_model.vector_quantizer.forward_idx(z)
+        idx = idx.squeeze(0).cpu().numpy()
+
+        # Transform into pseudo-tokens.
+        tokens = [f'{g1}-{g2}' for g1, g2 in idx]
+        sent = ' '.join(tokens)
+        tokens = roberta_model.encoder.dictionary.encode_line(
+            sent, append_eos=False, add_if_not_exist=False)
+        tokens = tokens.long().unsqueeze(0)
+        tokens = tokens.to(device)
+
+        # Run through RoBERTa.
+        feats, _ = roberta_model.encoder.extract_features(tokens)
+        feats = feats.T  # For some reason Roberta feats are inverted.
+
+    return feats.cpu().numpy()
+
+
+def extract_wav2vec2_feats(audio_path, model):
+    """Extract context aggregation layer feats from wav2vec 2.0 model."""
+    x = load_audio(audio_path, device=get_device(model))
+    with torch.no_grad():
+        handle = model.w2v_encoder.final_dropout.register_forward_hook(
+            get_activation('final_dropout'))
+        _ = model(source=x, padding_mask=None)
+        feats = activation['final_dropout']
+        handle.remove()
+    return feats.cpu().numpy().T
+
+
 def extract_feats_to_file(npy_path, audio_path, wav2vec_model,
-                          roberta_model=None, v2=False):
+                          roberta_model=None, wav2vec2=False):
     """Extract features to file.
 
     Parameters
@@ -92,94 +151,65 @@ def extract_feats_to_file(npy_path, audio_path, wav2vec_model,
 
     roberta_model : fairseq.models.RobertaModel, optional
         Roberta model.
-        (Parameters: None)
+        (Default: None)
+
+    wav2vec2 : bool, optional
+
     """
-    dev = get_device(wav2vec_model)
-
-    # Convert to 16 kHz.
-    x, sr = librosa.load(audio_path, sr=16000)
-    x = torch.from_numpy(x).unsqueeze(0)
-    x = x.to(dev)
-
-    # Extract context layer features from wav2vec model.
-    if not v2:
-        with torch.no_grad():
-            z = wav2vec_model.feature_extractor(x)
-            c = wav2vec_model.feature_aggregator(z)
-            feats = c
-
-        # If a RoBERTa model is specified, perform quantization
-        # and pass through RoBERTa to get features.
-        # https://github.com/pytorch/fairseq/issues/1793
-        if roberta_model is not None:
-            _, idx = wav2vec_model.vector_quantizer.forward_idx(z)
-            idx = idx.squeeze(0).cpu().numpy()
-            tokens = [f'{g1}-{g2}' for g1, g2 in idx]
-            sent = ' '.join(tokens)
-            tokens = roberta_model.encoder.dictionary.encode_line(
-                sent, append_eos=False, add_if_not_exist=False)
-            tokens = tokens.long().unsqueeze(0)
-            tokens = tokens.to(dev)
-            with torch.no_grad():
-                feats, _ = roberta_model.encoder.extract_features(tokens)
-            feats = feats.T  # For some reason Roberta feats are inverted.
-
-        # Save as uncompressed .npy file.
-        feats = feats.cpu().numpy()  # Shape: feat_dim x n_frames x 1
-        feats = np.squeeze(feats)
-        feats = feats.T  # Shape: n_frames x feat_dim
-        np.save(npy_path, feats)
-
-    # If a wav2vec 2.0 model is specified
+    # Extract frame-level features.
+    if not wav2vec2:
+        if roberta_model is None:
+            # wav2vec.
+            feats = extract_wav2vec_feats(audio_path, wav2vec_model)
+        else:
+            # vq-wav2vec + RoBERTa.
+            feats = extract_vqwav2vec_feats(
+                audio_path, wav2vec_model, roberta_model)
     else:
-        with torch.no_grad():
-            handle = \
-                wav2vec_model.w2v_encoder.final_dropout.register_forward_hook(
-                    get_activation('final_dropout'))
-            _ = wav2vec_model(source=x, padding_mask=None)
-            feats = activation['final_dropout']
-            handle.remove()
+        # wav2vec 2.0.
+        feats = extract_wav2vec2_feats(audio_path, wav2vec_model)
+    feats = np.squeeze(feats)
+    feats = feats.T  # Shape: n_frames x feat_dim
 
-            # feats = wav2vec_model.extract_features(
-            #     source=x, padding_mask=None)['encoder_out']
-
-        # Save as uncompressed .npy file.
-        feats = feats.cpu().numpy()  # Shape: n_frames x 1 x feat_dim
-        feats = np.squeeze(feats)  # Shape: n_frames x feat_dim
-        np.save(npy_path, feats)
+    # Save.
+    np.save(npy_path, feats)
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description='generate wav2vec/vq-wav2vec features', add_help=True)
+        description='generate wav2vec/vq-wav2vec/wav2vec 2.0 features',
+        add_help=True)
     parser.add_argument(
-        '--roberta', default=None, type=Path, metavar='STR',
-        help='path to RoBERTa checkpoint (default: %(default)s)')
+        'model', type=Path, help='wav2vec/vq-wav2vec/wav2vec 2.0 checkpoint')
     parser.add_argument(
-        '--vocab', default=None, type=Path, metavar='STR',
-        help='path to vocabulary file (default: %(default)s)')
-    parser.add_argument(
-        '--v2', default=False, action='store_true',
-        help='use wav2vec 2.0 model')
-    parser.add_argument(
-        '--use_gpu', default=False, action='store_true',
-        help='use GPU')
-    parser.add_argument(
-        'modelf', type=Path, help='wav2vec/vq-wav2vec/wav2vec 2.0 checkpoint')
-    parser.add_argument(
-        'feats_dir', type=Path,
+        'feats_dir', metavar='feats-dir',  type=Path,
         help='path to output directory for .npy files')
     parser.add_argument(
         'afs', nargs='*', type=Path, help='audio files to be processed')
+    parser.add_argument(
+        '--roberta', default=None, type=Path, metavar='CHECKPOINT',
+        help='path to RoBERTa checkpoint (default: %(default)s)')
+    parser.add_argument(
+        '--vocab', default=None, type=Path, metavar='VOCAB',
+        help='path to RoBERTa vocabulary file (default: %(default)s)')
+    parser.add_argument(
+        '--wav2vec2', default=False, action='store_true',
+        help='use wav2vec 2.0 model')
+    parser.add_argument(
+        '--use-gpu', default=False, action='store_true', help='use GPU')
+    parser.add_argument(
+        '--disable-progress', default=False, action='store_true',
+        help='disable progress bar')
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
     args = parser.parse_args()
 
-    if args.roberta and args.v2:
-        parser.error('Can\'t load vq-wav2vec and wav2vec 2.0 model together!')
+    # Validate command-line flags.
+    if args.roberta and args.wav2vec2:
+        parser.error("RoBERTa unavailable for wav2vec 2.0.")
 
-    os.makedirs(args.feats_dir, exist_ok=True)
+    args.feats_dir.mkdir(parents=True, exist_ok=True)
 
     # Determine device for computation.
     use_gpu = args.use_gpu and torch.cuda.is_available()
@@ -202,43 +232,37 @@ def main():
         roberta_model.eval()
         roberta_model = roberta_model.to(device)
 
-    # Load wav2vec 2.0 model..
-    using_wav2vec2 = args.v2 and args.roberta is None and \
-        args.vocab is not None
+    # Load wav2vec model.
+    using_wav2vec2 = args.wav2vec2 and not using_roberta
     if using_wav2vec2:
-        modelf = str(args.modelf)
-        model, cfg, task = \
-            fairseq.checkpoint_utils.load_model_ensemble_and_task(
-                [modelf], arg_overrides={"data": args.vocab})
+        # wav2vec 2.0.
+        model, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task(
+            [str(args.model)], arg_overrides={"data": args.vocab.parent})
         wav2vec_model = model[0]
         wav2vec_model.eval()
         wav2vec_model = wav2vec_model.to(device)
-
-    # Load wav2vec model..
-    if not using_wav2vec2:
-        wav2vec_cp = torch.load(args.modelf, map_location=torch.device('cpu'))
+    else:
+        # Vanilla wav2vec.
+        wav2vec_cp = torch.load(args.model, map_location=torch.device('cpu'))
         with pipes() as (stderr, stdout):
             wav2vec_model = Wav2VecModel.build_model(
                 wav2vec_cp['args'], task=None)
-
         wav2vec_model.load_state_dict(wav2vec_cp['model'])
         wav2vec_model.eval()
         wav2vec_model = wav2vec_model.to(device)
 
     # Process.
-    with tqdm(total=len(args.afs)) as pbar:
+    with tqdm(total=len(args.afs), disable=args.disable_progress) as pbar:
         for fn in args.afs:
             npy_path = Path(args.feats_dir, fn.stem + '.npy')
             try:
                 extract_feats_to_file(
                     npy_path, fn, wav2vec_model, roberta_model,
-                    v2=using_wav2vec2)
+                    wav2vec2=using_wav2vec2)
             except RuntimeError:
-                tqdm.write(
-                    f'ERROR: CUDA OOM error when processing "{fn}". \
-                      Skipping.')
+                tqdm.write(f'ERROR: CUDA OOM error when processing "{fn}". '
+                           f'Skipping.')
                 torch.cuda.empty_cache()
-
             pbar.update(1)
 
 
